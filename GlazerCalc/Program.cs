c891de7ce57a84d266251709a996194f2489c571
Marissa <marissacurry809@gmail.com>
2025-04-27 19:49:54 -0700

    all code

---
+++ GlazerCalc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+class GlazerCalc
+{
+    static void Main()
+    {
+        double width, height, woodLength, glassArea;
+
+        string widthString, heightString;
+
+        Console.WriteLine(" Lets calcualte the  area of the woodlenght for the glass");
+
+        Console.WriteLine("Enter the width");
+
+        widthString = Console.ReadLine();
+
+        width= double.Parse(widthString);
+
+
+        Console.WriteLine("Enter the the height");
+
+        heightString=Console.ReadLine();
+    
+        height= double.Parse(heightString);
+
+         woodLength= 2* (width +height) *3.25;
+
+        glassArea= 2* ( width+ height) * 3.25;
+
+
+        Console.WriteLine($"The length of the wood is " + woodLength+"feet");
+
+        Console.WriteLine($"The area of the glass is "+ glassArea +"squaremetres");
+
+
+    }
+}
